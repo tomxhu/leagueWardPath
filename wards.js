87d60d95289a37c1b06749ebce8c415166b922ea
Tommy Hu <tomxhu@gmail.com>
2015-03-19 03:51:12 +0100

    added LolAPI

--- wards.js
+++ wards.js
@@ -1,12 +1,21 @@
-var _ = require('./underscore');
+var _ = require('underscore')
 var file = require('./1748270655.json');
 
-// gets each frame of the event
-//console.log(file.timeline.frames[15]);
+var LolApi = require('leagueapi');
 
-// console.log(file.timeline.frames[15].events);
+LolApi.init('f0a8b929-dbf5-402f-b553-89bd0c34b649', 'na');
 
-var frames = file.timeline.frames;
+var matches;
+
+var options = {rankedQueues: ['RANKED_SOLO_5x5']};
+LolApi.getMatchHistory(126443, options, 'na', function(err, data){
+	console.log(data);
+	matches = data;
+});
+
+console.log(matches)
+
+//var frames = file.timeline.frames;
 
 var locArr = [];
 
@@ -23,10 +32,9 @@ function findPosOfEvents(frames) {
 			});
 		}
 	});
-	
 }
 
-findPosOfEvents(frames)
+// findPosOfEvents(frames)
 
-console.log(locArr.length);
+// console.log(_.compact(locArr).length);
 
