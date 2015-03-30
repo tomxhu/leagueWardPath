6766a7aafaa711cb24e4f777de2cecf0b5a67127
Tommy Hu <tomxhu@gmail.com>
2015-03-30 11:58:03 -0400

    get positions plus list of next summoners

--- wards.js
+++ wards.js
@@ -1,4 +1,5 @@
-var _ = require('underscore')
+var _ = require('underscore');
+var Q = require('q');
 var file = require('./1748270655.json');
 
 
@@ -19,6 +20,8 @@ var PosSchema = new Schema({
 	}
 });
 
+var myId = 126443;
+
 var PositionModel = mongoose.model('Position', PosSchema);
 
 var LolApi = require('leagueapi');
@@ -27,28 +30,48 @@ LolApi.init('f0a8b929-dbf5-402f-b553-89bd0c34b649', 'na');
 
 var matches;
 
-var options = {rankedQueues: ['RANKED_SOLO_5x5']};
-LolApi.getMatchHistory(126443, options, 'na', function(err, data){
-	data.matches.forEach(function(match){
-
-	});
-});
-
-
-
-function getPositionFromMatch(matchID){
-	LolApi.getMatch(matchID, true, 'na', function(err, data){
-		findPosOfEvents(data.timeline.frames);
-	});
+function treeSearchMatches(seedID, levelsDeep){
+	if (levelsDeep > 0){
+		getFirstMatchID(seedID).then(function(matchID){
+			console.log(matchID);
+			LolApi.getMatch(matchID, true, 'na', function(err, data){
+				findPosOfEvents(data);
+				var nextSumonners = _.reject(getListOfSummonerIDs(data), function(player){
+					return player.id == seedID;
+				});
+				console.log(nextSumonners);
+			});
+		})
+	}
 }
 
+function getFirstMatchID(summonerID){
+	var deferred = Q.defer();
+	var options = {rankedQueues: ['RANKED_SOLO_5x5']};
+	LolApi.getMatchHistory(summonerID, options, 'na', function(err, data){
+		deferred.resolve(data.matches[0].matchId);
+	});
+	return deferred.promise;
+}
 
+function getListOfSummonerIDs(match) {
+	var summonerIDs = []
 
-//var frames = file.timeline.frames;
+	match.participantIdentities.forEach(function(player){
+		var temp = {
+			name: player.player.summonerName,
+			id: player.player.summonerId
+		}
+		summonerIDs.push(temp)
+		
+	});
+	return summonerIDs;
+}
 
-var locArr = [];
+treeSearchMatches(myId, 1);
 
-function findPosOfEvents(frames) {
+function findPosOfEvents(match) {
+	var frames = match.timeline.frames
 	frames.forEach(function(frame){
 		var events = frame.events;
 		var participantFrames = frame.participantFrames;
@@ -57,27 +80,27 @@ function findPosOfEvents(frames) {
 				if (event.eventType == 'WARD_PLACED'){
 					var wardingPlayer = event.creatorId;
 					var pos = participantFrames[wardingPlayer].position;
-					console.log(pos.x);
+					console.log(pos);
 
 					var posEntry = new PositionModel({
 						x: pos.x,
 						y: pos.y
 					});
 
-					posEntry.save(function(err){
-						if (err) {
-							console.log(err);
-						} else {
-							return console.log('saved');
-						}
-					});
-				}
-			});
+					// posEntry.save(function(err){
+					// 	if (err) {
+					// 		console.log(err);
+					// 	} else {
+					// 		// return console.log('saved');
+					// 	}
+					// });
+		}
+	});
 		}
 	});
 }
 
 // findPosOfEvents(frames)
-getPositionFromMatch(1750903595);
+// getPositionFromMatch(1750903595);
 
 
